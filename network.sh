ad6a0f56be3d2d38d021789c51d883a6bc957661
jjackban <bjh0921@gmail.com>
2025-05-17 02:07:43 +0900

    feat: add production code

--- network.sh
+++ network.sh
@@ -2,12 +2,17 @@
 
 function generateCert() {
  # 인증서 dev 모드 생성
- basic-network/scripts/generateCert.sh
+ basic-network/scripts/generateCert.sh $1
 }
 
 function runCAdev() {
  # CA dev 생성
  basic-network/scripts/runCAdev.sh
+}
+
+function runCAOrg3() {
+ # CA dev 생성
+ basic-network/scripts/runCAOrg3.sh
 }
 
 function cleanNetwork() {
@@ -21,7 +26,7 @@ function upNetwork() {
 }
 function createConfigtxgen() {
  # 채널 정보 생성
- basic-network/scripts/createConfigtxgen.sh
+ basic-network/scripts/createConfigtxgen.sh $1
 }
 
 function joinChannel() {
@@ -49,42 +54,62 @@ function upgradeCC() {
  docker exec cli scripts/upgradeCC.sh $1 $2
 }
 if [ "$1" == "generateCert" ]; then
- generateCert org1peer0 orderer
+ generateCert $2
 elif [ "$1" == "createConfigtxgen" ]; then
- createConfigtxgen
+ createConfigtxgen $2
 elif [ "$1" == "upNetwork" ]; then
- upNetwork org1peer0 orderer
+ upNetwork $2
 elif [ "$1" == "createChannel" ]; then
  joinChannel createChannel
 elif [ "$1" == "joinChannel" ]; then
  joinChannel joinChannel
+elif [ "$1" == "joinChannelProd" ]; then
+ joinChannel joinChannelProd
 elif [ "$1" == "updateAnchor" ]; then
  joinChannel updateAnchor
+elif [ "$1" == "updateAnchorProd" ]; then
+ joinChannel updateAnchorProd
 elif [ "$1" == "installCC" ]; then
  installCC $2
 elif [ "$1" == "checkCC" ]; then
  checkCC $2
 elif [ "$1" == "runCAdev" ]; then
  runCAdev
+elif [ "$1" == "runCAOrg3" ]; then
+ runCAOrg3
 elif [ "$1" == "startSDK" ]; then
  startSDK
 elif [ "$1" == "upgradeCC" ]; then
  upgradeCC $2 $3
 elif [ "$1" == "clean" ]; then
  cleanNetwork
-elif [ "$1" == "start" ]; then
- generateCert
+elif [ "$1" == "dev" ]; then
+ generateCert dev
  sleep 2
- createConfigtxgen
+ createConfigtxgen dev
  sleep 2
- upNetwork org1peer0 orderer
+ upNetwork dev
  sleep 2
  joinChannel createChannel
  joinChannel joinChannel
  joinChannel updateAnchor
  sleep 2
  runCAdev
-
+elif [ "$1" == "prod" ]; then
+ generateCert prod
+ sleep 2
+ runCAOrg3
+ sleep 2
+ createConfigtxgen prod
+ sleep 2
+ upNetwork prod
+ sleep 2
+ sleep 2
+ joinChannel createChannel
+ joinChannel joinChannelProd
+ joinChannel updateAnchorProd
+ sleep 2
+ runCAdev
 else
  echo -n "unknown parameter"
  exit 1
